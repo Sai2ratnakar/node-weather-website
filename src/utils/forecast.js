e09bce35294a4d16a823bfd2e0de0972b8318289
Sai Alapati <sai2ratnakar@gmail.com>
2019-10-28 02:55:51 +1100

    Fixed vulnerabilties

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const forecast = (lat, lon, callback) => {
     const url = 'https://api.darksky.net/forecast/37a5c957e1fcb8d4553ce67ac9184b13/'+ lat +',' + lon +'?units=si'
-    console.log(url);
+    // console.log(url);
     request({url, json: true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather services', undefined)
